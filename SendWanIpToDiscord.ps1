265c07f39c1f546688a3dad4498206ea7bf04189
RobOderWas <165394530+RobOderWas@users.noreply.github.com>
2024-06-19 18:28:54 +0200

    Add files via upload

---
+++ SendWanIpToDiscord.ps1
@@ -0,0 +1,29 @@
+﻿try {
+$wanIP = (Invoke-RestMethod -Uri "http://ifconfig.me/ip").Trim()
+
+$hookUrl = 'WEBHOOK URL' #Webhook URL muss hier eingetragen werden
+
+$port = 25565
+
+$content = @"
+> @everyone
+# Aktuelle Minecraft Server Addresse:
+``${wanIP}:${port}``
+"@
+
+
+$payload = [PSCustomObject]@{
+
+    content = $content
+
+}
+
+Invoke-RestMethod -Uri $hookUrl -Method Post -ContentType 'Application/Json' -Body ($payload | ConvertTo-Json -Compress)
+
+Write-Host "Nachricht erfolgreich an den Discord-Webhook gesendet."
+
+} catch {
+
+Write-Host "Bro idk iwas hat ned gfetzt"
+
+}
